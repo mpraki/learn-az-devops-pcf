31f2de06cfe18557ff0d02b21a6691dc4d9e0b15
Mani <374702@cognizant.com>
2020-03-16 11:48:17 +0530

    Initial Commit

---
+++ src/main/java/com/example/learnazdevopspcf/LearnAzDevopsPcfApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnazdevopspcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAzDevopsPcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAzDevopsPcfApplication.class, args);
+	}
+
+}
